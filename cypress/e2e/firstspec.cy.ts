3b3d2e7db3c8b4f82fe019135e06f9ab20919c06
Nikola Pepic <nikola.pepic@netsetglobal.rs>
2022-12-06 11:35:41 +0100

    Cypress yml push

---
+++ cypress/e2e/firstspec.cy.ts
@@ -0,0 +1,8 @@
+/// <reference types="cypress"/>
+
+describe("empty spec", () => {
+  it("passes", () => {
+    cy.visit("https://example.cypress.io");
+    cy.url().should("contain", "example");
+  });
+});
