61a057f932140d76cd44fa15f6bc29017d22edab
Nikola Pepic <nikola.pepic@netsetglobal.rs>
2022-12-13 10:46:11 +0100

    Dodat h1 test

--- cypress/e2e/firstspec.cy.ts
+++ cypress/e2e/firstspec.cy.ts
@@ -5,4 +5,8 @@ describe("Tests on CI/CD", () => {
     cy.visit("https://example.cypress.io");
     cy.url().should("contain", "example").and("contain", "cypress");
   });
+  it("page is loaded", () => {
+    cy.visit("http://localhost:3000");
+    cy.get("h1").should("have.text", "Sudoku");
+  });
 });
